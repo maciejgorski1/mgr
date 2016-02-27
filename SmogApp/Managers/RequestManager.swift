a470d77f8a808722eda1107cfa4f9df53f4e4683
Myrenkar <myrenkar@mbp-myrenkar.mshome.net>
2016-02-27 11:37:15 +0100

    added swRevealViewCont4roller

---
+++ SmogApp/Managers/RequestManager.swift
@@ -0,0 +1,13 @@
+//
+//  RequestManager.swift
+//  SmogApp
+//
+//  Created by Myrenkar on 27.02.2016.
+//  Copyright © 2016 moo. All rights reserved.
+//
+
+import UIKit
+
+class RequestManager: NSObject {
+
+}
