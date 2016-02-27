b3884d811dca435b314bf4a90bd8aa4076a27c7c
Myrenkar <myrenkar@MacBook-Pro-Myrenkar.local>
2016-02-27 10:31:16 +0100

    Initial commit

---
+++ SmogApp/Pods/Target Support Files/Pods-SmogApp/Pods-SmogApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmogApp : NSObject
+@end
+@implementation PodsDummy_Pods_SmogApp
+@end
---
+++ SmogApp/Pods/Target Support Files/Pods-SmogApp/Pods-SmogApp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SmogAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SmogAppVersionString[];
+
