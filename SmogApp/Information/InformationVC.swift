0ffae28fa8952cc360e42ba9cf0c29e13b016fb0
Myrenkar <myrenkar@macbook-pro-myrenkar.local>
2016-02-28 12:23:25 +0100

    Adding storyboards

---
+++ SmogApp/Information/InformationVC.swift
@@ -0,0 +1,21 @@
+//
+//  InformationVC.swift
+//  SmogApp
+//
+//  Created by Myrenkar on 28.02.2016.
+//  Copyright © 2016 moo. All rights reserved.
+//
+
+import UIKit
+
+class InformationVC: UIViewController {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
