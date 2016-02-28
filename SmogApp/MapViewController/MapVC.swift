c114ad97ddb1c246e22fa455dd6eaaf455696b13
Myrenkar <myrenkar@macbook-pro-myrenkar.local>
2016-02-28 11:38:12 +0100

    Added menu storyboard

---
+++ SmogApp/MapViewController/MapVC.swift
@@ -0,0 +1,21 @@
+//
+//  MapVC.swift
+//  SmogApp
+//
+//  Created by Myrenkar on 28.02.2016.
+//  Copyright © 2016 moo. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
