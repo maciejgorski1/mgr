8939da218aa854f3a85aaf95282d659c48322e3a
Myrenkar <myrenkar@macbook-pro-myrenkar.local>
2016-03-23 22:38:39 +0100

    dodano mape google do projektu

--- SmogApp/AppDelegate.swift
+++ SmogApp/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyDSD_EDnw9Ipz8D88vc8blO5vcPul_OGKI")
+
         return true
     }
 
@@ -40,7 +42,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
--- SmogApp/MapViewController/MapVC.swift
+++ SmogApp/MapViewController/MapVC.swift
@@ -7,25 +7,38 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapVC: UIViewController {
 
+    // google map api AIzaSyDSD_EDnw9Ipz8D88vc8blO5vcPul_OGKI
+
     @IBOutlet weak var menuButton: UIBarButtonItem!
     /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-    
-    
+     // Only override drawRect: if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func drawRect(rect: CGRect) {
+     // Drawing code
+     }
+     */
+
     override func viewDidLoad() {
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
-    }
 
+        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
 }
