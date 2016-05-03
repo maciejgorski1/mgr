45e88c43ee52883fdb28957848e73bfd2592cb70
Myrenkar <myrenkar@macbook-pro-myrenkar.local>
2016-05-03 23:53:34 +0200

    alerty

---
+++ SmogApp/SettingsViewController/Cell/AlertTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlertTableViewCell.swift
+//  SmogApp
+//
+//  Created by Myrenkar on 03.05.2016.
+//  Copyright © 2016 moo. All rights reserved.
+//
+
+import UIKit
+
+class AlertTableViewCell: UITableViewCell {
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var textlabel: UILabel!
+
+}
