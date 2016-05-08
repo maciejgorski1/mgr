31b343efd06d9181f35e7ccdac656c1b306a5b6c
Myrenkar <myrenkar@macbook-pro-myrenkar.local>
2016-05-08 15:41:58 +0200

    zmiany

--- SmogApp/Information/InformationVC.swift
+++ SmogApp/Information/InformationVC.swift
@@ -28,12 +28,36 @@ class InformationVC: UIViewController {
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
 
-//        self.coButton.layer
+        self.coButton.layer.cornerRadius = self.coButton.frame.height / 2
+        self.coButton.layer.masksToBounds = true
 
         let requestObj = NSURLRequest(URL: myURL!)
         myWebView.loadRequest(requestObj)
         self.view.addSubview(myWebView)
+        
+        setUpAppeareance()
     }
+    func setUpAppeareance(){
+        self.coButton.layer.cornerRadius = self.coButton.frame.height / 2
+        self.coButton.layer.masksToBounds = true
+        
+        self.so2Button.layer.cornerRadius = self.so2Button.frame.height / 2
+        self.so2Button.layer.masksToBounds = true
+        
+        self.pm10Button.layer.cornerRadius = self.pm10Button.frame.height / 2
+        self.pm10Button.layer.masksToBounds = true
+        
+        self.noxButton.layer.cornerRadius = self.noxButton.frame.height / 2
+        self.noxButton.layer.masksToBounds = true
+        
+        self.benzenButton.layer.cornerRadius = self.benzenButton.frame.height / 2
+        self.benzenButton.layer.masksToBounds = true
+        
+        self.ozonButton.layer.cornerRadius = self.ozonButton.frame.height / 2
+        self.ozonButton.layer.masksToBounds = true
+    }
+    
+    
 
     @IBAction func coButtonClicked(sender: AnyObject) {
         myURL = NSBundle.mainBundle().URLForResource("co", withExtension: "html")
